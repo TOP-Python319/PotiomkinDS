765ffb97ac3b54a3c7e1c58aaa9efca89a151ab3
Maks Iamchuk <maks.iamchuk@outlook.com>
2024-02-22 17:53:24 +0400

    Домашнее задание 2 принято и проверено

--- HW_2024_02_09/ex4.py
+++ HW_2024_02_09/ex4.py
@@ -14,3 +14,17 @@ else:
 # Введите координаты первой клетки: f5
 # Введите координаты второй клетки: a5
 # нет
+
+# можно добавить ещё переменных, чтобы код выглядел более осмысленным
+# условие сразу читается гораздо легче
+
+# first_letter = ord(coord1[0])
+# first_digit = int(coord1[1])
+# second_letter = ord(coord2[0])
+# second_digit = int(coord2[1])
+
+# if (((first_letter + first_digit) % 2 == 0 and (second_letter + second_digit) % 2 == 0) 
+#     or ((first_letter + first_digit) % 2 == 1 and (second_letter + second_digit) % 2 == 1)):
+#     print ('да')
+# else: 
+#     print ('нет')
